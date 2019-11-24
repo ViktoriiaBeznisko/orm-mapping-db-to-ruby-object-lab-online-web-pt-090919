a887eb2195f3b600c0e2702d900917bd8a34087e
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-11-24 05:58:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,12 +82,12 @@ class Student
     end
   end
   
-    def self.first_X_students_in_grade_10(students)
+    def self.first_X_students_in_grade_1
     sql = <<-SQL
       SELECT * 
       FROM students
       WHERE grade < 10
-      LIMIT 10
+      LIMIT ?
     SQL
     
     DB[:conn].execute(sql).map do |row|

ec452b130ec209ebf8828ecc9615731b90f31f9a
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-11-24 05:49:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -40,7 +40,7 @@ class Student
       WHERE grade = 9
     SQL
     
-    DB[:conn].execute(sql, grade). do |row|
+    DB[:conn].execute(sql, grade).collect do |row|
       self.new_from_db(row)
     end
   end

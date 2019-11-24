a320f360b01575a1c1e33742192ec709edc51a6b
Viktoriia Beznisko <viktoriiabeznisko@gmail.com>
2019-11-24 05:54:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,11 +58,11 @@ class Student
     end.first
   end
   
-    def self.all_students_in_grade_X
+    def self.all_students_in_grade_X(grade)
     sql = <<-SQL
       SELECT * 
       FROM students
-      WHERE grade = 10
+      WHERE grade = ?
     SQL
     
     DB[:conn].execute(sql).map do |row|
